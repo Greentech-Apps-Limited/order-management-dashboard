3d5c468af190da001635328c65dcfda786b9d096
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2023-11-27 12:56:20 +0600

    cusror update

--- components/user-menu.jsx
+++ components/user-menu.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const UserMenu = () => {
   return (
-    <div className="flex items-center justify-start px-4 py-2 rounded-full bg-gray hover:bg-gray-200">
+    <div className="flex items-center justify-start px-4 py-2 rounded-full cursor-pointer bg-gray hover:bg-gray-200">
       <p>Maksudul Hasan</p>
       <ArrowDownIcon className="ml-2 text-2xl" />
     </div>
