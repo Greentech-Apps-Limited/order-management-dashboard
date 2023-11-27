ec40806a7b771059d3d421a67edfa7077f044280
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2023-11-27 11:26:40 +0600

    notification button added

--- components/header-mobile.jsx
+++ components/header-mobile.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Notification from './notification';
 
 const HeaderMobile = () => {
   return (
-    <div className="h-16 px-4 border-b border-gray-200 md:hidden bg-neutral">HeaderMobile</div>
+    <div className="flex items-center justify-between h-16 px-4 border-b border-gray-200 md:hidden bg-neutral">
+      HeaderMobile
+      <div>
+        <Notification  />
+      </div>
+    </div>
   );
 };
 
--- components/notification.jsx
+++ components/notification.jsx
@@ -1,7 +1,14 @@
+import { NotificationIcon } from '@/icons';
 import React from 'react';
 
 const Notification = () => {
-  return <div>Notification</div>;
+  return (
+    <div>
+      <button type="button" aria-label="Notification" className="p-2 rounded-full bg-gray">
+        <NotificationIcon className="text-2xl" />
+      </button>
+    </div>
+  );
 };
 
 export default Notification;
