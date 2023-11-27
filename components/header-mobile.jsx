6e1d067f52791c39f83600a7db9c5cbc220c03d4
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2023-11-27 11:58:11 +0600

    toggle menu funtionality

--- components/header-mobile.jsx
+++ components/header-mobile.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import Image from 'next/image';
 
 import { MenuIcon } from '@/icons';
+import useUtility from '@/hooks/useUtilityContext';
 
 import Notification from './notification';
 
 const HeaderMobile = () => {
+  const { toggleSidebar } = useUtility();
   return (
     <div className="flex items-center justify-between h-16 px-4 border-b border-gray-200 md:hidden bg-neutral">
       <div>
-        <button type="button" aria-label="Menu">
+        <button type="button" aria-label="Toggle menu" onClick={toggleSidebar}>
           <MenuIcon className="text-2xl" />
         </button>
       </div>
