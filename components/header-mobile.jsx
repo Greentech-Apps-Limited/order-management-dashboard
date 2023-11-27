b94674603b0842ea989fab1fb3406670ec05e6aa
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2023-11-27 11:32:24 +0600

    menu included

--- components/header-mobile.jsx
+++ components/header-mobile.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { MenuIcon } from '@/icons';
 import Notification from './notification';
 
 const HeaderMobile = () => {
   return (
     <div className="flex items-center justify-between h-16 px-4 border-b border-gray-200 md:hidden bg-neutral">
-      HeaderMobile
       <div>
-        <Notification  />
+        <button type="button" aria-label="Menu">
+          <MenuIcon className="text-2xl" />
+        </button>
+      </div>
+      <div>
+        <Notification />
       </div>
     </div>
   );
