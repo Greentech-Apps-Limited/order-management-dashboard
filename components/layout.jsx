80ff6b1efd2a15029469460612aaa7b2868a528c
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2023-11-27 13:13:55 +0600

    demo sidebar added

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import Header from './header';
 import HeaderMobile from './header-mobile';
+import SideBar from './side-bar';
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col h-screen">
-      <div className="z-40">
-        <Header />
-        <HeaderMobile />
+    <div className="flex w-full h-screen">
+      <div className="hidden md:block">
+        <SideBar />
       </div>
-      <div className="flex-1 overflow-y-auto">{children}</div>
+      <div className="flex flex-col w-full h-screen">
+        <div className="z-40">
+          <Header />
+          <HeaderMobile />
+        </div>
+        <div className="flex-1 overflow-y-auto">{children}</div>
+      </div>
     </div>
   );
 };
