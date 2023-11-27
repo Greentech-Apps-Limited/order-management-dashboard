0ff41b0b5c422c552db82db966f076e657e1f120
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2023-11-27 12:33:38 +0600

    search bar component added

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import SearchBar from './search-bar';
 
 const Header = () => {
   return (
-    <div className="items-center hidden h-20 px-4 border-b border-gray-200 md:flex bg-neutral">
-      Header
+    <div className="items-center justify-between hidden h-20 px-4 border-b border-gray-200 md:flex bg-neutral">
+      <div className="w-72">
+        <SearchBar />
+      </div>
     </div>
   );
 };
