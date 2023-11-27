6c2866c81fae9ae49380d45b00f9c4ebfa164d22
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2023-11-27 11:40:58 +0600

    project logo added

--- components/header-mobile.jsx
+++ components/header-mobile.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import Image from 'next/image';
+
 import { MenuIcon } from '@/icons';
+
 import Notification from './notification';
 
 const HeaderMobile = () => {
@@ -9,6 +12,9 @@ const HeaderMobile = () => {
         <button type="button" aria-label="Menu">
           <MenuIcon className="text-2xl" />
         </button>
+      </div>
+      <div className="w-max">
+        <Image src="/images/project-logo.svg" alt="logo" width={32} height={32} />
       </div>
       <div>
         <Notification />
