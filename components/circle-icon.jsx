d83c2db5e85bed0581fa1d00eb93ce576cbae5ac
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2023-11-30 16:47:20 +0600

    add reusable circle-icon component

---
+++ components/circle-icon.jsx
@@ -0,0 +1,17 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+const CircleIcon = ({ children, className }) => {
+  const circleIconClasses = cn(
+    'rounded-full',
+    'bg-gray-100',
+    'text-2xl',
+    'text-primary-8',
+    'p-2',
+    className
+  );
+
+  return <div className={circleIconClasses}>{children}</div>;
+};
+
+export default CircleIcon;
