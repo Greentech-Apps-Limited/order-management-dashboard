d1fe120bd3ab89e81dd70369176d36fdda3e8e3d
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2023-11-30 22:31:41 +0600

    add loading skeleton and uuid function

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,12 @@
 import { getOrderStatistics } from '@/apiService';
 import Layout from '@/components/layout';
 import OrderStatusCard from '@/components/order-status-card';
+import OrderStatusCardSkeleton from '@/components/order-status-card-skeleton';
 import { transformOrderData } from '@/lib/utils';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [orderStat, setOrderState] = useState([]);
-  const [customerReviewData, setCustomerReviewData] = useState({});
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -14,10 +14,9 @@ export default function Home() {
       setLoading(true);
       try {
         const response = await getOrderStatistics();
-        const { order, customer } = response.data;
+        const { order } = response.data;
         const modifiedData = transformOrderData(order);
         setOrderState(modifiedData);
-        setCustomerReviewData(customer);
       } finally {
         setLoading(false);
       }
@@ -30,22 +29,26 @@ export default function Home() {
       <Layout>
         <div className="p-4">
           <h1 className="text-xl">Overview</h1>
-          <div className="grid grid-cols-2">
-            <section className="grid grid-cols-2 gap-4 mt-4">
-              {orderStat.map((orderStatus) => {
-                const { title, icon, value, percentage, isPositivePercentage } = orderStatus;
-                return (
-                  <OrderStatusCard
-                    key={orderStatus.title}
-                    title={title}
-                    icon={icon}
-                    value={value}
-                    percentage={percentage}
-                    isPositivePercentage={isPositivePercentage}
-                  />
-                );
-              })}
-            </section>
+          <div className="grid grid-cols-1 gap-4 mt-4 xl:grid-cols-2">
+            {loading ? (
+              <OrderStatusCardSkeleton />
+            ) : (
+              <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 ">
+                {orderStat.map((orderStatus) => {
+                  const { title, icon, value, percentage, isPositivePercentage } = orderStatus;
+                  return (
+                    <OrderStatusCard
+                      key={orderStatus.title}
+                      title={title}
+                      icon={icon}
+                      value={value}
+                      percentage={percentage}
+                      isPositivePercentage={isPositivePercentage}
+                    />
+                  );
+                })}
+              </section>
+            )}
           </div>
         </div>
       </Layout>
