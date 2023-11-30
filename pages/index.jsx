2aae4e7df927d16fe22b6ad978442074514d8d8a
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2023-11-30 20:34:36 +0600

    fix key related wornings

--- pages/index.jsx
+++ pages/index.jsx
@@ -25,6 +25,7 @@ export default function Home() {
                 const { title, icon, value, percentage, isPositivePercentage } = orderStatus;
                 return (
                   <OrderStatusCard
+                    key={orderStatus.title}
                     title={title}
                     icon={icon}
                     value={value}
