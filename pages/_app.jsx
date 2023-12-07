2bb5408df8de850e115998154e413fae53c492d9
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2023-12-07 20:41:12 +0600

    add fav icon and update title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,16 @@
 import '@/styles/globals.css';
 import { satoshi } from '@/lib/font';
 import { UtilityProvider } from '@/contexts/utility-context';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <UtilityProvider>
+        <Head>
+          <title>GreenTech | Order Management System</title>
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" sizes="32x32" />
+        </Head>
         <main className={`${satoshi.variable} font-satoshi`}>
           <Component {...pageProps} />
         </main>
