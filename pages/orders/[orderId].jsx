a7b13fc7f2619ea405d0c37b78ad7211c286e3f5
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2023-12-07 09:16:25 +0600

    call api for order details and get dynamic data

--- pages/orders/[orderId].jsx
+++ pages/orders/[orderId].jsx
@@ -1,7 +1,31 @@
+import { getOrderDetails } from '@/apiService';
 import Layout from '@/components/layout';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
 
 const OrderDetails = () => {
+  const {
+    query: { orderId },
+  } = useRouter();
+
+  const [orderDetails, setOrderDetails] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const fetchOrderDetails = async () => {
+    setLoading((prev) => ({ ...prev, details: true }));
+
+    try {
+      const response = await getOrderDetails(orderId);
+      setOrderDetails(response.data);
+    } finally {
+      setLoading((prev) => ({ ...prev, details: false }));
+    }
+  };
+
+  useEffect(() => {
+    if (orderId) fetchOrderDetails();
+  }, [orderId]);
+
   return (
     <main>
       <Layout>Order details</Layout>
