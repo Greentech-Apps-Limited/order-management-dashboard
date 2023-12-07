a2c6ae5bf8aad7020d86608ba82567ec0a40e556
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2023-12-07 14:02:37 +0600

    update dependent components placement

--- components/order-list.jsx
+++ components/order-list.jsx
@@ -7,45 +7,6 @@ import { useRouter } from 'next/router';
 import Checkbox from './checkbox';
 import OrderStatusBadge from './order-status-badge';
 
-const OrderList = ({ orders }) => {
-  const tableHeadTitle = [
-    'Order no.',
-    'Item Details',
-    'Package type',
-    'Qty.',
-    'Weight',
-    'Status',
-    'Expected Delivery date',
-  ];
-  return (
-    <div className="w-full">
-      <div className="my-4 overflow-x-auto border border-gray-200 rounded-xl">
-        <table className="w-full text-left text-gray-500 bg-neutral">
-          <thead className="text-sm ">
-            <tr>
-              <th scope="col" className="p-2 w-max">
-                <Checkbox label="Select all" />
-              </th>
-              {tableHeadTitle.map((title) => (
-                <th key={title} scope="col" className="p-2 text-sm font-normal text-start ">
-                  <div className="min-w-max"> {title}</div>
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {orders.map((order) => {
-              return <OrderItem key={order.id} order={order} />;
-            })}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
-};
-
-export default OrderList;
-
 const OrderItem = ({ order }) => {
   const statusType = ORDER_STATUS_TYPE[order.status];
   const { push } = useRouter();
@@ -114,3 +75,42 @@ const ItemShortDetails = ({ itemDetails }) => {
     </div>
   );
 };
+
+const OrderList = ({ orders }) => {
+  const tableHeadTitle = [
+    'Order no.',
+    'Item Details',
+    'Package type',
+    'Qty.',
+    'Weight',
+    'Status',
+    'Expected Delivery date',
+  ];
+  return (
+    <div className="w-full">
+      <div className="my-4 overflow-x-auto border border-gray-200 rounded-xl">
+        <table className="w-full text-left text-gray-500 bg-neutral">
+          <thead className="text-sm ">
+            <tr>
+              <th scope="col" className="p-2 w-max">
+                <Checkbox label="Select all" />
+              </th>
+              {tableHeadTitle.map((title) => (
+                <th key={title} scope="col" className="p-2 text-sm font-normal text-start ">
+                  <div className="min-w-max"> {title}</div>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => {
+              return <OrderItem key={order.id} order={order} />;
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default OrderList;
