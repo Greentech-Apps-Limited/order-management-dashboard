13ce3bb29709ad94bd379c36a491523510ae50cf
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2023-12-07 17:13:31 +0600

    update order list table and remove checkbox

--- components/order-list.jsx
+++ components/order-list.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { formatDate } from '@/lib/utils';
 import { ORDER_STATUS_TYPE } from '@/lib/constants';
 import { useRouter } from 'next/router';
-import Checkbox from './checkbox';
 import OrderStatusBadge from './order-status-badge';
 import ItemDetails from './item-details';
 
@@ -17,9 +16,6 @@ const OrderItem = ({ order }) => {
       onClick={handleNavigation}
       className="text-gray-800 border-t border-gray-200 cursor-pointer text-start hover:bg-gray rounded-xl"
     >
-      <td className="p-2 min-w-max">
-        <Checkbox label="Select item" />
-      </td>
       <td className="p-2">
         <div className="min-w-max"> {order.id}</div>
       </td>
@@ -61,9 +57,6 @@ const OrderList = ({ orders }) => {
         <table className="w-full text-left text-gray-500 bg-neutral">
           <thead className="text-sm ">
             <tr>
-              <th scope="col" className="p-2 w-max">
-                <Checkbox label="Select all" />
-              </th>
               {tableHeadTitle.map((title) => (
                 <th key={title} scope="col" className="p-2 text-sm font-normal text-start ">
                   <div className="min-w-max"> {title}</div>
